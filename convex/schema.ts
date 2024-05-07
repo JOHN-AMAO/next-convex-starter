22e36080d78f228d5a6d0a5d16b5593560961cb3
JOHN-AMAO <amaojohn767@gmail.com>
2024-05-07 20:02:19 +0100

    .

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,7 +2,7 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  User: defineTable({
+  Users: defineTable({
     userId: v.string(),
     userType: v.string(),
   }),
